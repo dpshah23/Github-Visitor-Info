9c3a47dcac904d64f48dced1aefaf96bb6f240f0
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-01 20:07:46 +0530

    Added logic to get ip and country

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ app=Flask(__name__)
 
 @app.route('/api/v1/track/<id>/',methods=['POST','GET'])
 def track(id):
-    return "Tracked"
+    ip_address = request.remote_addr
+    print(ip_address)
+    location_info = requests.get(f"https://ipinfo.io/{ip_address}/json").json()
+    country = location_info.get('country')
+    city = location_info.get('city')
+
+    print(country,city)
+    return ""
+    
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
    
